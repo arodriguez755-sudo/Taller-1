76f07eba91dea3896d6a61b26d837518d9816331
arodriguez755-sudo <arodriguez755@uan.edu.co>
2026-09-24 12:39:48 -0500

    Update Ejercicio 18.cpp

--- Ejercicio 18.cpp
+++ Ejercicio 18.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+
+int main() {
+    double num1, num2, num3;
+
+ 
+    std::cout << "Ingrese el primer número: ";
+    std::cin >> num1;
+    std::cout << "Ingrese el segundo número: ";
+    std::cin >> num2;
+    std::cout << "Ingrese el tercer número: ";
+    std::cin >> num3;
+
+  
+    double mayor = num1;
+
+    if (num2 > mayor) {
+        mayor = num2;
+    }
+    if (num3 > mayor) {
+        mayor = num3;
+    }
+
+
+    std::cout << "El mayor de los tres números es: " << mayor << std::endl;
+
+    return 0;
+}
 
