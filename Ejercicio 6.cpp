4c7cf427afa3a03b4e442adc410b5963bd66bb14
arodriguez755-sudo <arodriguez755@uan.edu.co>
2026-09-24 13:02:14 -0500

    Create Ejercicio 6.cpp

---
+++ Ejercicio 6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+
+    // Lectura de los dos números enteros
+    cout << "Ingrese el primer numero entero (x): ";
+    cin >> x;
+
+    cout << "Ingrese el segundo numero entero (y): ";
+    cin >> y;
+
+    // Muestra de los valores ingresados
+    cout << "\nLos valores ingresados son:" << endl;
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+
+    return 0;
+}
