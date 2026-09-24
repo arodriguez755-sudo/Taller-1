355bd043e5018a0ac2ae32e29a49603d6f3213cd
arodriguez755-sudo <arodriguez755@uan.edu.co>
2026-09-24 12:34:56 -0500

    Create Ejercicio 4.cpp

---
+++ Ejercicio 4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ int main() {
+     
+     float x,r;
+     
+     cout<<"Ingresa el numero entero";
+     cin>>x;
+     
+     r = (x*2);
+     
+     cout<<"El resultado es: "<<r<<endl;
+     
+ }
